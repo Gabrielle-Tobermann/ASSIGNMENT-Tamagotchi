9f2aac5f1b4c58b30ef76cf95f0b32c2be9c206f
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-02-25 21:04:20 -0600

    buttons work

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,8 +1,31 @@
 import '../styles/main.scss';
+import printToDom from './helpers/printToDom';
+
+let full = 100;
+const healthy = (e) => {
+  if (e.target.id === 'healthy-btn') {
+    full += 10;
+  }
+  document.querySelector('#full').innerHTML = full;
+};
+
+const unhealthy = (e) => {
+  if (e.target.id === 'unhealthy-btn') {
+    full -= 3;
+  }
+  document.querySelector('#full').innerHTML = full;
+};
+
+const eatButtons = () => {
+  const domString = `<div id="full">${full}</div><div><button type="button" class="btn btn-primary" id="healthy-btn">Healthy food</button><button type="button" class="btn btn-primary" id="unhealthy-btn">Unhealthy food</button></div>`;
+
+  printToDom('#eat', domString);
+  document.querySelector('#healthy-btn').addEventListener('click', healthy);
+  document.querySelector('#unhealthy-btn').addEventListener('click', unhealthy);
+};
 
 const init = () => {
-  $('#app').html('<h1>HELLO! You are up and running!</h1>');
-  console.log('YOU ARE UP AND RUNNING!');
+  eatButtons();
 };
 
 init();
