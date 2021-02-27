683052ec9bd11508180686ed76d612ee1eb6b623
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-02-27 00:23:30 -0600

    refactored code to be reusable

---
+++ src/javascripts/helpers/data/eatData.js
@@ -0,0 +1,23 @@
+const full = [100];
+const healthy = (e) => {
+  if (e.target.id === 'healthy-food') {
+    const lastIndex = full[full.length - 1];
+    let newValue = lastIndex + 10;
+    if (newValue > 100) {
+      newValue = 100;
+    }
+    full.push(newValue);
+    document.querySelector('#score--Fullness').innerHTML = `Fullness Score: ${full[full.length - 1]}`;
+  }
+};
+
+const unhealthy = (e) => {
+  if (e.target.id === 'unhealthy-food') {
+    const lastIndex = full[full.length - 1];
+    const newValue = lastIndex - 3;
+    full.push(newValue);
+    document.querySelector('#score--Fullness').innerHTML = `Fullness Score: ${full[full.length - 1]}`;
+  }
+};
+
+export { full, healthy, unhealthy };
