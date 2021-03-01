b35cbe7129a52a963bf63c631e27a55f64079115
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-03-01 15:33:48 -0600

    progress bar updates

--- src/javascripts/helpers/data/progressData.js
+++ src/javascripts/helpers/data/progressData.js
@@ -7,7 +7,7 @@ const progressUpdate = (e) => {
   let average = 0;
   if (e.target.type === 'button') {
     average = (full[full.length - 1] + fun[fun.length - 1] + strength[strength.length - 1] + energy[energy.length - 1]) / 4;
-    document.querySelector('#progress').innerHTML = `<div class="progress"><div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="${average}" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div></div>`;
+    document.querySelector('#progress').innerHTML = `<div class="progress"><div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="${average}" aria-valuemin="0" aria-valuemax="100" style="width: ${average}%"></div></div>`;
     console.warn(average);
   }
 };
