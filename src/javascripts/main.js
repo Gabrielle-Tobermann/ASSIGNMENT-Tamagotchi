3b61b9edea6c30a42517fc2ba7919d57b6f73cec
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-02-28 21:42:09 -0600

    added basic styling

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -11,7 +11,7 @@ import { sleepQuadrant, nap, deepSlumber } from './helpers/data/sleepData';
 
 const quadBuilder = (arr, i, divId, callback1, callback2) => {
   let domString = '';
-  domString += `<div id ="name--${arr[i].name}">${arr[i].name}</div><div><div id="score--${arr[i].score.type}">${arr[i].score.type} Score: ${arr[i].score.val} </div><button type="button" class="btn btn-primary" id="${arr[i].button1.id}">${arr[i].button1.label}</button><button type="button" class="btn btn-primary" id="${arr[i].button2.id}">${arr[i].button2.label}</button></div>`;
+  domString += `<div id ="name--${arr[i].name}">${arr[i].name}</div><div><div id="score--${arr[i].score.type}">${arr[i].score.type} Score: ${arr[i].score.val} </div><button type="button" class="btn btn-outline-dark btn-sm m-2 rounded-0" id="${arr[i].button1.id}">${arr[i].button1.label}</button><button type="button" class="btn btn-outline-dark btn-sm m-2 rounded-0" id="${arr[i].button2.id}">${arr[i].button2.label}</button></div>`;
   printToDom(divId, domString);
   document.querySelector(`#${arr[i].button1.id}`).addEventListener('click', callback1);
   document.querySelector(`#${arr[i].button2.id}`).addEventListener('click', callback2);
