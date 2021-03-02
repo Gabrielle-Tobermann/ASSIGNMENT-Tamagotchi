1b8424074b675c59363d934a55c99c6b9d8bd174
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-03-02 13:07:13 -0600

    progress bar color changes

--- src/javascripts/components/progressBuilder.js
+++ src/javascripts/components/progressBuilder.js
@@ -1,7 +1,7 @@
 import printToDom from '../helpers/printToDom';
 
 const progressBuilder = () => {
-  const domString = '<div class="progress"><div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div></div>';
+  const domString = '<div id= progress-msg>Progress</div><div class="progress"><div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div></div>';
   printToDom('#progress', domString);
 };
 
--- src/javascripts/helpers/data/progressData.js
+++ src/javascripts/helpers/data/progressData.js
@@ -7,8 +7,15 @@ const progressUpdate = (e) => {
   let average = 0;
   if (e.target.type === 'button') {
     average = (full[full.length - 1] + fun[fun.length - 1] + strength[strength.length - 1] + energy[energy.length - 1]) / 4;
-    document.querySelector('#progress').innerHTML = `<div class="progress"><div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="${average}" aria-valuemin="0" aria-valuemax="100" style="width: ${average}%"></div></div>`;
-    console.warn(average);
+    if (average >= 75) {
+      document.querySelector('#progress').innerHTML = `<div id= progress-msg>Progress</div><div class="progress"><div class="progress-bar progress-bar-striped progress-bar-animated bg-success" role="progressbar" aria-valuenow="${average}" aria-valuemin="0" aria-valuemax="100" style="width: ${average}%"></div></div>`;
+    } else if (average > 50 && average < 75) {
+      document.querySelector('#progress').innerHTML = `<div id= progress-msg>Progress</div><div class="progress"><div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="${average}" aria-valuemin="0" aria-valuemax="100" style="width: ${average}%"></div></div>`;
+    } else if (average > 25 && average <= 50) {
+      document.querySelector('#progress').innerHTML = `<div id= progress-msg>Progress</div><div class="progress"><div class="progress-bar progress-bar-striped progress-bar-animated bg-warning" role="progressbar" aria-valuenow="${average}" aria-valuemin="0" aria-valuemax="100" style="width: ${average}%"></div></div>`;
+    } else if (average <= 25) {
+      document.querySelector('#progress').innerHTML = `<div id= progress-msg>Progress</div><div class="progress"><div class="progress-bar progress-bar-striped progress-bar-animated bg-danger" role="progressbar" aria-valuenow="${average}" aria-valuemin="0" aria-valuemax="100" style="width: ${average}%"></div></div>`;
+    }
   }
 };
 
